b35cb56760ba88c67a33010112a14945dce23b41
Wayaer <wayaer@foxmail.com>
2020-01-09 11:51:21 +0800

    push

--- src/native/AliPay.ts
+++ src/native/AliPay.ts
@@ -1,26 +1,27 @@
 type Status = '9000' | '8000' | '4000' | '5000' | '6001' | '6002';
 
 interface Result {
-  resultStatus: Status;
-  result: string;
-  memo: string;
+    resultStatus: Status;
+    result: string;
+    memo: string;
 }
 
 export declare namespace AliPay {
 
-  function aliPayAuthWithInfo(infoStr: string): Result;
+    function aliPayAuthWithInfo(infoStr: string): Result;
 
-  // 设置沙箱模式
-  function setAliPaySandbox(isSandbox: boolean): void
+    // 设置沙箱模式
+    function setAliPaySandbox(isSandbox: boolean): void
 
-  function aliPay(infoStr: string): Result;
+    function aliPay(infoStr: string): Result;
 
-  function getAliPaySDKVersion(): Result;
+    function getAliPaySDKVersion(): Result;
 
-  function aliPayInterceptorWithUrl(infoStr: string): {
-    resultCode: Status;
-    returnUrl: string;
-  };
+    function aliPayInterceptorWithUrl(infoStr: string): {
+        resultCode: Status;
+        returnUrl: string;
+    };
 
-  // function sign(infoStr: string): string;
+    // function sign(infoStr: string): string;
 }
+
