4a1afddc63b5afa172512f619fb271d05c831950
Wayaer <wayaer@foxmail.com>
2021-01-25 18:07:07 +0800

    no message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import RNFetchBlob from 'rn-fetch-blob';
 
 export * from './src/constant';
+export * from './src/storage';
 export * from './src/linear_gradient';
 export * from './src/upload_utils';
 export * from './src/native_utils';
